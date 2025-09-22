35a4f0d4da04b61e6b2b3dbcf3dd1a1386a39723
Nilesh Singhania <47456990+nil0481@users.noreply.github.com>
2025-09-22 13:01:46 +0530

    Update settings.py

--- excel_chat/excel_chat/settings.py
+++ excel_chat/excel_chat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hzonh-_d_cg5-%pl+0klje@*rvjp4!e2!tdopn)42hmjj!#8h_'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
